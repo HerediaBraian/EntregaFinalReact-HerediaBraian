8e2ed6d739b9ec6e20b66b677ee371a92deaa5eb
francisco garao <garaofran@gmail.com>
2024-02-27 11:09:45 -0300

    orders

--- PreEntregaReact1/src/pages/orders/CreateOrder.jsx
+++ PreEntregaReact1/src/pages/orders/CreateOrder.jsx
@@ -7,7 +7,9 @@ export default function CreateOrder({cart}){
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [payMethod, setPayMethod] = useState('');
-    console.log({"Carringui???": cart});
+    const [showOrder, setShowOrder] = useState(false);
+    const [order, setOrder] = useState({});
+    
     const handleSubmitName = (e) => {
         setName(e.target.value);
     } 
@@ -39,9 +41,15 @@ export default function CreateOrder({cart}){
         const db =  getFirestore();
         const colRef =  collection(db, 'orders');
         await addDoc(colRef, order).then((res)=>res)
+        setOrder(order);
+        console.log({"order":order});
         cart = [];
         console.log('Orden creada con exito');
-        window.location.reload();
+        setShowOrder(true)
+        console.log(showOrder);
+        setTimeout(() => {
+            window.location.reload();
+        }, 10000);
     } 
     
    const cancelOrder = () => {
@@ -52,6 +60,8 @@ export default function CreateOrder({cart}){
     return (
         <>
         <form className='confirm-form' action="">
+        {         !showOrder ? (
+        <>
         <h1>Confirma tu compra</h1>
             <input type="text"  placeholder='Nombre' onChange={handleSubmitName}/><input placeholder='Apellido'  onChange={handleSubmitLastName} type="text" />
             <input placeholder='Email' onChange={handleSubmitEmail} type="text" />
@@ -64,6 +74,32 @@ export default function CreateOrder({cart}){
             <button onClick={createOrder}>Confirmar Compra</button>
             <button onClick={cancelOrder}>Cancelar Compra</button>
             </div>
+            </>)
+ :          (
+            <>
+            <h1>Orden creada con exito</h1>
+            <div>
+            <p>Datos Comprador: {order.buyer.name}</p>
+            <p>Nombre Completo: {order.buyer.lastName}</p>
+            <p>Email: {order.buyer.email}</p>
+            <p>Metodo de pago: {order.buyer.payMethod}</p>
+            <p>Datos Compra: </p>
+            <div className='order-products'>
+            {order.cart.products.map(product => {
+                return (
+                    <div className='order-products-item' key={product.id}>
+                    <p>Producto: {product.title}</p>
+                    <p>Precio: {product.price}</p>
+                    <p>Categoria: {product.category}</p>
+                    </div>
+                )
+            })}
+            </div>
+            <p>Precio Final: {order.cart.total}</p>
+
+            </div>
+            </>
+    )        }
         </form>
         </>
     )
